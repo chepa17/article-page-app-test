a56ebe2d4e52b68c9b37e75b5fe596f5a5fac41f
chepa17 <chepkiyn@gmail.com>
2022-10-27 15:08:56 +0200

    feat: add types

---
+++ src/types/insex.ts
@@ -0,0 +1,29 @@
+export type ArticleType = {
+    articleId: string;
+    articleUrl: string;
+    averageRating: string;
+    expectedDeliveryDate: string;
+    images: ArticleImage[];
+    oldPrice: Price;
+    price: Price;
+    productId: string;
+    shippingTimeMax: string;
+    shippingTimeMin: string;
+    slug: string;
+    title: string;
+}
+
+export type ApiResponse = {
+    articles: ArticleType[];
+}
+
+export type ArticleImage = {
+    cloudinaryId: string;
+    url: string;
+}
+
+export type Price = {
+    amount: number;
+    currency: string;
+    formatted: string;
+}
